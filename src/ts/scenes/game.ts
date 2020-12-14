c3fa21d751335af1a8f8b27a857f02b25afd3b77
Sergey Tokarev <sergey.zoomaster@gmail.com>
2020-12-14 15:41:58 +0700

    сетка кнопок в сцене игры

--- src/ts/scenes/game.ts
+++ src/ts/scenes/game.ts
@@ -16,19 +16,41 @@ export class Game extends Phaser.Scene {
     this.cameras.main.setBackgroundColor('#DEDEDE');
     console.log('start game');
 
-    const but1 = this.add.text(0, 0, 'Svg2Texture', {
-      fontFamily: 'Open Sans',
-      fontSize: '32px',
-    });
-    ButtonFabric.makeButton(but1, () => {
-      const key = 'Svg2TextureScene';
-      this.addAndStartScene(key, Svg2TextureScene);
+    const buttons = [
+      this.addSceneButton('Svg2Texture', 'Svg2TextureScene', Svg2TextureScene),
+      this.addSceneButton('Svg2Texture', 'Svg2TextureScene', Svg2TextureScene),
+      this.addSceneButton('Svg2Texture', 'Svg2TextureScene', Svg2TextureScene),
+      this.addSceneButton('Svg2Texture', 'Svg2TextureScene', Svg2TextureScene),
+    ];
+
+    Phaser.Actions.GridAlign(buttons, {
+      x: 150,
+      y: 90,
+      width: 3,
+      cellWidth: 250,
+      cellHeight: 100,
     });
   }
 
-  private addAndStartScene(key: string, scene: new () => Phaser.Scene) {
+  private addSceneButton(text: string, sceneKey: string, sceneClass: new () => Phaser.Scene) {
+    const button = this.add.text(0, 0, 'Svg2Texture', {
+      fontFamily: 'Verdana',
+      fontSize: '32px',
+      backgroundColor: 'gold',
+      color: 'black',
+      padding: {
+        x: 10,
+        y: 10,
+      },
+    });
+    button.setOrigin(0.5, 0.5);
+    ButtonFabric.makeButton(button, () => this.addAndStartScene(sceneKey, sceneClass));
+    return button;
+  }
+
+  private addAndStartScene(key: string, sceneClass: new () => Phaser.Scene) {
     const isExist = this.scene.get(key) != null;
-    if (!isExist) this.scene.add(key, new scene(), false);
+    if (!isExist) this.scene.add(key, new sceneClass(), false);
     this.scene.start(key);
   }
 }
